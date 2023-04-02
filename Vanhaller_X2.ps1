4cebffae2d88e8178d0a64f4687c026803a67ba0
umbraexy <101243382+umbraexy@users.noreply.github.com>
2023-04-02 07:38:34 -0400

    Add files via upload

---
+++ Vanhaller_X2.ps1
@@ -0,0 +1,11 @@
+﻿#2/23/23
+#Created by umbraexy
+
+Get-NetTCPConnection -State Listen, ESTABLISHED | Select-Object -Expandproperty OwningProcess | Sort-Object -Unique -OutVariable id | out-null;
+$ciminfo= foreach ($line in $id) {
+    if ($line -match $regex) {
+        Get-CimInstance -Class Win32_Process | where-object PROCESSID -eq $line | Select-Object -Property ProcessId, ProcessName, CommandLine, Path, ParentProcessId, CreationDate
+    }
+};
+$ciminfo > list_proc_cim_obj.txt;
+$ciminfo | select-object -Expandproperty ProcessName | Get-Unique -AsString > list_proc_names.txt;
